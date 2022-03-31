7de95b2c310fb4d67c37cd2e0eab3a80fec92aae
JustTheInstinct <jordangmain@gmail.com>
2022-03-31 14:34:10 -0700

    TARGET_ENV fix typo

--- Reciever/app.py
+++ Reciever/app.py
@@ -11,7 +11,7 @@ from pykafka import KafkaClient
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     print("In Test Environment")
-    app_conf_file = "/config/reciever/app_conf.yaml"
+    app_conf_file = "/config/receiver/app_conf.yaml"
     log_conf_file = "/config/receiver/log_conf.yaml"
 else:
     print("In Dev Environment")
